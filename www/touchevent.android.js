0df6e93f5346f61453d3369db0ef170c1b0c5408
Hans Krauss <hanskrauss8674@gmail.com>
2015-01-22 11:41:50 -0800

    Update touchevent.android.js

--- www/touchevent.android.js
+++ www/touchevent.android.js
@@ -2,7 +2,7 @@ var exec = require('cordova/exec'),
     touchEvent = {};
 
 touchEvent.fireAt = function(x, y) {
-    exec(null, null, "HoodalabsTouchEvent", "touchEvent", ['fireat', x, y]);
+    exec(null, null, "HoodalabsTouchEvent", "fireAt", [x, y]);
 };
 
 module.exports = touchEvent;
