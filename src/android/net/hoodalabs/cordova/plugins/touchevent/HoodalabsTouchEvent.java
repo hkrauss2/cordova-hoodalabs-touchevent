2972c2ebda85fe8c468ee75bc19b537e7f984bb4
Hans Krauss <hanskrauss8674@gmail.com>
2015-01-22 11:39:57 -0800

    Update HoodalabsTouchEvent.java

--- src/android/net/hoodalabs/cordova/plugins/touchevent/HoodalabsTouchEvent.java
+++ src/android/net/hoodalabs/cordova/plugins/touchevent/HoodalabsTouchEvent.java
@@ -21,3 +21,63 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
+package net.hoodalabs.cordova.plugins.touchevent;
+
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.os.SystemClock;
+import android.view.InputDevice;
+import android.view.MotionEvent;
+
+public class HoodalabsTouchEvent extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+
+        if ("fireAt".equals(action)) {
+
+            final float x = args.getInt(0);
+            final float y = args.getInt(1);
+
+            // List of meta states found here: developer.android.com/reference/android/view/KeyEvent.html#getMetaState()
+            final int metaState = 0;
+
+            final long timestamp = SystemClock.uptimeMillis();
+
+            MotionEvent touchStart = MotionEvent.obtain(
+                    timestamp,
+                    timestamp + 50,
+                    MotionEvent.ACTION_DOWN,
+                    x,
+                    y,
+                    metaState
+            );
+            touchStart.setSource(InputDevice.SOURCE_TOUCHSCREEN);
+
+            webView.dispatchTouchEvent(touchStart);
+
+            MotionEvent touchEnd = MotionEvent.obtain(
+                    timestamp+50,
+                    timestamp+100,
+                    MotionEvent.ACTION_UP,
+                    x,
+                    y,
+                    metaState
+            );
+            touchEnd.setSource(InputDevice.SOURCE_TOUCHSCREEN);
+
+            webView.dispatchTouchEvent(touchEnd);
+
+
+            callbackContext.success(); // Thread-safe.
+            return true;
+        }
+
+        return false;
+    }
+
+}
